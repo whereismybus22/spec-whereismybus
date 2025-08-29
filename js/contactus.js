a28a241bc74e7a27a14aedd4c98b73988a00bbba
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2025-08-29 13:52:04 +0530

    Update contactus.js

--- js/contactus.js
+++ js/contactus.js
@@ -33,9 +33,9 @@ function sendContactForm(event) {
   const issue = issueSelect.options[issueSelect.selectedIndex].text;
   const description = document.getElementById("query").value.trim();
   const os = getMobileOS();
-  const college = "Sreeja International School";
+  const college = "St peters college of Engineering";
   const subject = issue;
-  const body = 'School : ' + college + '\n' +
+  const body = 'Institution : ' + college + '\n' +
                'Mobile OS : ' + os + '\n' +
                'Name : ' + name + '\n' +
                'Mobile Number : ' + phone + '\n' +
